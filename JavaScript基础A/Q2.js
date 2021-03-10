b3d44b18755755283b178bcee9619128d6a82b5d
cuihaoran <1054346513@qq.com>
2021-03-10 10:03:36 +0800

    题目新增

--- JavaScript基础A/Q2.js
+++ JavaScript基础A/Q2.js
@@ -1,3 +1,6 @@
+/**
+ * 简单版本
+ */
 this.a = 20;
 function go() {
   console.log(this.a);
@@ -12,7 +15,53 @@ var test = {
     return fn;
   }
 };
-console.log((new go()).a);  // 
+console.log((new go()).a);
 test.init(go);
-var p = test.init(go);
+var p = test.init(go);  
 p();
+
+// 答案：40 30 20 50 30 50 30
+// 解题思路：this的指向问题   this.a的多次变化
+
+console.log('*********************************')
+/**
+ * 加强版
+ */
+var num = 1;
+function yideng() {
+  "use strict";
+  console.log(this.num++);
+}
+function yideng2() {
+  console.log(++this.num);
+}
+(function() {
+  "use strict";
+  yideng2();
+})();
+// yideng();
+
+// 答案：2 Cannot read property 'num' of undefined
+// "use strict"：禁止this关键字指向全局对象
+
+/**
+ * 扩展题目
+ */
+console.log('*************扩展题目************');
+function C1(name) {
+  if(name) this.name = name;
+}
+
+function C2(name) {
+  this.name = name;
+}
+
+function C3(name) {
+  this.name = name || 'fe';
+}
+C1.prototype.name = "yideng";
+C2.prototype.name = "lao";
+C3.prototype.name = "yuan";
+console.log((new C1().name) + (new C2().name) + (new C3().name));
+
+// 答案：yidengundefinedfe
---
+++ JavaScript基础A/Q4.js
@@ -0,0 +1,6 @@
+function test(m) {
+  m = { v: 5 }
+}
+var m = { k: 30 };
+test(m);
+console.log(m.v);
